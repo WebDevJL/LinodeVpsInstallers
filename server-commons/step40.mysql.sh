6fb86a6f9a61aeeddc50602847429a3095c8d0e7
Jérémie Litzler <contact@jeremielitzler.net>
2017-08-11 06:39:23 +0200

    script tweaks

--- server-commons/step40.mysql.sh
+++ server-commons/step40.mysql.sh
@@ -2,7 +2,7 @@
 echo "Install of  MySQL"
 sudo apt-get install mysql-server
 sudo mysql_secure_installation
-sudo cp /etc/mysql/mysql.conf.d/mysqld.conf /etc/mysql/mysql.conf.d/mysqld.conf.bak
+#sudo cp /etc/mysql/mysql.conf.d/mysqld.conf /etc/mysql/mysql.conf.d/mysqld.conf.bak
 sudo cp /vps-manager/assets/mysqld.conf /etc/mysql/mysql.conf.d/mysqld.conf
 sudo service mysql restart
 echo "Logging in mysql-server"
