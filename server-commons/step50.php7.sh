44c48d904bc55f45df5748172c81ff5cb728c9cf
Jérémie Litzler <contact@puzzlout.com>
2017-08-14 08:06:34 +0200

    Update step50.php7.sh

--- server-commons/step50.php7.sh
+++ server-commons/step50.php7.sh
@@ -5,6 +5,7 @@ sudo apt-get install php7.0-curl php7.0-json php7.0-cgi
 cp /etc/php/7.0/apache2/php.ini /etc/php/7.0/apache2/php.bak.ini
 vim /etc/php/7.0/apache2/php.ini
 # see assets/php.conf
+cp /etc/php/7.0/apache2/php.ini vps-manager/assets/php/php.ini.base
 sudo mkdir /var/log/php
 sudo chown www-data /var/log/php
 echo "Reloading apache2..."
