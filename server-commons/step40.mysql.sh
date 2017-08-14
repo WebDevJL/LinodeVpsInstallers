3b5d465acd44fede3bdee7d6fee8dcd9cc20ed4f
Jérémie Litzler <contact@puzzlout.com>
2017-08-14 08:04:58 +0200

    Update step40.mysql.sh

--- server-commons/step40.mysql.sh
+++ server-commons/step40.mysql.sh
@@ -3,7 +3,7 @@ echo "Install of  MySQL"
 sudo apt-get install mysql-server
 sudo mysql_secure_installation
 #sudo cp /etc/mysql/mysql.conf.d/mysqld.conf /etc/mysql/mysql.conf.d/mysqld.conf.bak
-sudo cp /vps-manager/assets/mysqld.conf /etc/mysql/mysql.conf.d/mysqld.conf
+sudo cp /vps-manager/assets/conf/mysql/mysqld.conf /etc/mysql/mysql.conf.d/mysqld.conf
 sudo service mysql restart
 echo "Logging in mysql-server"
 echo "(Type exit when you are done)"
