d5b197e8257f2b4e0a6c9b64b7ae0b2df7dbfbb4
Jérémie Litzler <contact@jeremielitzler.net>
2017-08-18 06:45:44 +0200

    adding webpack

--- server-commons/step80.extra.utils.sh
+++ server-commons/step80.extra.utils.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Install of  Node, NPM, Yarn"
+echo "Install of  Node, NPM, Yarn and webpack"
 sudo apt-get install nodejs
 sudo apt-get install npm
 # Bower has become obsolete
@@ -9,3 +9,4 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
 yarn --version
+yarn add webpack --dev
