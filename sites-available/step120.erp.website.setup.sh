2c0c4b50c48c0cca9d7598b42f4cab344203cd6b
Jérémie Litzler <contact@puzzlout.com>
2017-08-18 14:31:04 +0200

    Update and rename step120.erp.sh to step120.erp.website.setup.sh

--- sites-available/step120.erp.website.setup.sh
+++ sites-available/step120.erp.website.setup.sh
@@ -18,15 +18,3 @@ sudo a2ensite $vDomain.conf
 sudo systemctl reload apache2
 cd ~
 cd ..
-
-#Install of  Dolibarr on erp.puzzlout.ovh
-echo "Install of  Dolibarr on $vDomain"
-cd /var/www/$vDomain/public_html
-composer create-project dolibarr/dolibarr erp
-sudo vim /etc/apache2/sites-available/erp.puzzlout.ovh.conf
-sudo vim /etc/apache2/sites-available/erp.puzzlout.ovh-le-ssl.conf
-sudo systemctl reload apache2
-sudo chmod -R 777 /var/www/erp.puzzlout.ovh/public_html/erp/
-echo "Follow instruction @ https://erp.puzzlout.ovh"
-cd ~
-cd ..
