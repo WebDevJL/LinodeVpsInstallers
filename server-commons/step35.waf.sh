794b64c4add5bf29de3db8afa7089f5d51e9d821
Jérémie Litzler <contact@puzzlout.com>
2017-08-24 14:01:48 +0200

    Update step35.waf.sh

--- server-commons/step35.waf.sh
+++ server-commons/step35.waf.sh
@@ -3,8 +3,14 @@ echo "Beginning Web Application Firewall (WAF) installation..."
 echo "Let's start with ModSecurity..."
 sudo apt-update
 # https://www.netnea.com/cms/apache-tutorial-6_embedding-modsecurity/
-sudo apt-get install libapache2-modsecurity
-apache2ctl -M | grep -i security
+sudo apt-get install libapache2-mod-security2 -y
+sudo apachectl -M | grep --color security2
+#sudo mv /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
+sudo service apache2 reload
+echo "Configure ModSecurity..."
+
+sudo cp /vps-manager/assets/conf/security2_module/modsecurity.conf /etc/modsecurity/modsecurity.conf
+sudo service apache2 reload
 read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
 
 echo "... and then ModSecurity CRS..."
