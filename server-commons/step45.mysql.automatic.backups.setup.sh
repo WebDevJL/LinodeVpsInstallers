c9d379781a262a901ff937698d0d805df24113af
Jérémie Litzler <contact@puzzlout.com>
2017-08-28 15:48:15 +0200

    Update step45.mysql.automatic.backups.setup.sh

--- server-commons/step45.mysql.automatic.backups.setup.sh
+++ server-commons/step45.mysql.automatic.backups.setup.sh
@@ -8,7 +8,9 @@ newDirForBackup=$( date '+%Y-%m-%d' )
 sudo mkdir $newDirForBackup
 cd $newDirForBackup
 mysqldump --all-databases > dump-$( date '+%Y-%m-%d_%H-%M-%S' ).sql -u root -p
-
+echo "Check the test backup was created... a sql file should be listed below..."
+ls -l
+read -n1 -rsp $'If you see "deflate_module (shared)", pPress any key to continue or Ctrl+C to exit...\n'
 echo "Set the cron task following the example below..."
 echo "0 1 * * * /usr/bin/mysqldump --all-databases > dump-$( date '+%Y-%m-%d_%H-%M-%S' ).sql -u root -pPASSWORD"
 cd ~
