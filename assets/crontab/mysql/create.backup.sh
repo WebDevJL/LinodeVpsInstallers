def1d344d8e64353ecc93050a523095833c178a5
Jérémie Litzler <contact@puzzlout.com>
2017-08-28 10:56:35 +0200

    Update and rename server-commons/step45.mysql.automatic.backups.cron.sh to assets/crontab/mysql/create.backup.sh

---
+++ assets/crontab/mysql/create.backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Perform a MySQL backup..."
+bash /var/mysql/backups/create.backup.sh
+newDirForBackup=$( date '+%Y-%m-%d' )
+sudo mkdir $newDirForBackup
+cd $newDirForBackup
+mysqldump --all-databases > dump-$( date '+%Y-%m-%d_%H-%M-%S' ).sql -u root -pPASSWORD
+cd ~
+echo "MySQL backup done!"
