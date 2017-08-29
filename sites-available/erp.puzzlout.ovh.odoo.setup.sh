fee7df7fd68f06b7460737cc023578b0d09bc22e
Jérémie Litzler <contact@puzzlout.com>
2017-08-29 13:43:41 +0200

    #10 - setup

--- sites-available/erp.puzzlout.ovh.odoo.setup.sh
+++ sites-available/erp.puzzlout.ovh.odoo.setup.sh
@@ -22,5 +22,4 @@ sudo npm install -g less less-plugin-clean-css
 cd /tmp
 sudo wget https://downloads.wkhtmltopdf.org/0.12/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
 sudo dpkg -i wkhtmltox-0.12.1_linux-trusty-amd64.deb
-sudo dpkg -i wkhtmltox-0.12.1_linux-trusty-amd64.deb
 sudo cp /usr/local/bin/wkhtmltoimage /usr/bin
