a44cf4ae72eae64c388f42e2320908b9a0f88d95
Jérémie Litzler <contact@puzzlout.com>
2017-08-29 15:22:22 +0200

    #10 - config

--- sites-available/erp.puzzlout.ovh.odoo.configuration.sh
+++ sites-available/erp.puzzlout.ovh.odoo.configuration.sh
@@ -1,2 +1,16 @@
 
 sudo cp /opt/odoo/debian/odoo.conf /etc/odoo-server.conf
+sudo vim /lib/systemd/system/odoo-server.service
+sudo chmod 755 /lib/systemd/system/odoo-server.service
+sudo chown root: /lib/systemd/system/odoo-server.service
+sudo chown -R odoo: /opt/odoo/
+sudo chown odoo:root /var/log/odoo
+sudo chown odoo: /etc/odoo-server.conf
+sudo chmod 640 /etc/odoo-server.conf
+sudo systemctl start odoo-server
+sudo systemctl status odoo-server
+sudo journalctl -u postgresql
+sudo systemctl status odoo-server
+sudo systemctl enable odoo-server
+# Reboot Linode VPS
+sudo journalctl -u odoo-server
