b97fcdc376df52bf7f26ad9502d584bb5a4481c4
Jérémie Litzler <contact@puzzlout.com>
2017-09-01 18:05:38 +0200

    bug fix - ssl cleanup

--- sites-available/common/website-ssl-clean.sh
+++ sites-available/common/website-ssl-clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Let's remove the SSL setup for $1..."
 sudo rm -R /etc/letsencrypt/live/$1
-sudo rm -R etc/letsencrypt/archive/$1
+sudo rm -R /etc/letsencrypt/archive/$1
 echo "SSL setup removed for $1. You regenerate with the command 'bash /vps-manager/sites-available/common/website-ssl-setup.sh example.com'"
