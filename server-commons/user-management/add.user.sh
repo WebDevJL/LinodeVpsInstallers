1962d8f99788db7250e866787580f24e3b350e98
Jérémie Litzler <contact@jeremielitzler.net>
2017-09-04 07:18:51 +0200

    rearranging scripts - add.user bug fix

--- server-commons/user-management/add.user.sh
+++ server-commons/user-management/add.user.sh
@@ -7,7 +7,7 @@ adduser $1 sudo
 echo "Creating web directory /home/$1/www..."
 mkdir /home/$1/www
 echo "... that $1:$2 owns!"
-chown -R $1:$2 /home/$vUser/www
+chown -R $1:$2 /home/$1/www
 echo "$1:$2 owns web directory /home/$1/www!"
 echo "Setting up the basics... User $1 created!"
 read -n1 -rsp $'If you see "deflate_module (shared)", pPress any key to continue or Ctrl+C to exit...\n'
