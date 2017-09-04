d9ffb030e544079abc66e4da6f1dbfbd737d0ba3
Jérémie Litzler <contact@puzzlout.com>
2017-09-04 15:43:57 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
+cd ~/vps-manager
+
 ##
 ## Server commons installs
 ##
-cd ~/vps-manager
+
 # Install the basics
 bash server-commons/basics.sh
-# Install of fail2ban
-bash server-commons/fail2ban.sh
-# Install of  Apache 2
-bash server-commons/apache2.setup.sh
-bash server-commons/apache2.cache.sh
-bash server-commons/apache2.compression.sh
-# Install of  MySQL
-bash server-commons/mysql.setup.sh
-# Mysql backups require a backup mysql user
-#bash server-commons/mysql.automatic.backups.sh
-# Install of  PHP7
-bash server-commons/php7.setup.sh
-# Check and modify UFW
-bash server-commons/ufw.config.sh
+# Install and configure the web server
+bash server-commons/web.parts.sh
 # Install of Composer
 bash server-commons/composer.setup.sh
 # Install of  Node, NPM, bower
 bash server-commons/extra.utils.sh
-# Install of HTTPS with Let's Encrypt
-bash server-commons/ssl.setup.sh
-# Install of  phpMyAdmin
-bash server-commons/phpmyadmin.setup.sh
 
 # Return to home dir of root user
 cd ~
