b3ae1783f9a2fc3583216c0621157247f05d1621
Jérémie Litzler <contact@puzzlout.com>
2017-09-04 15:21:31 +0200

    Update fail2ban.sh

--- server-commons/fail2ban.sh
+++ server-commons/fail2ban.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd ~/vps-manager
+
 echo "Install of fail2ban..."
 apt-get update && apt-get upgrade -y
 apt-get install fail2ban
@@ -7,9 +9,9 @@ ufw allow ssh
 ufw enable
 cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
 echo "Configuring fail2ban..."
-cp /vps-manager/assets/conf/fail2ban/jail.local /etc/fail2ban/jail.local
+cp ~/vps-manager/assets/conf/fail2ban/jail.local /etc/fail2ban/jail.local
 #cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
-#cp /etc/fail2ban/jail.local /vps-manager/assets/conf/fail2ban/jail.local
+#cp /etc/fail2ban/jail.local ~/vps-manager/assets/conf/fail2ban/jail.local
 vim /etc/fail2ban/jail.local
 fail2ban-client status
 sudo netstat -tulpn
