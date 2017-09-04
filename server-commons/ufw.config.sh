4b433842426d418735b1a7a863c29030a7c4f967
Jérémie Litzler <contact@puzzlout.com>
2017-09-04 15:28:00 +0200

    Update ufw.config.sh

--- server-commons/ufw.config.sh
+++ server-commons/ufw.config.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+cd ~/vps-manager
+
 echo "Check and modify UFW"
 sudo ufw status
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 sudo ufw enable
 echo "Done with ufw modification."
+
+cd ~
