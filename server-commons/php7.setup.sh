cbb6f664e0e62925e50aab624a972956337527f6
Jérémie Litzler <contact@puzzlout.com>
2017-09-04 15:25:50 +0200

    Update php7.setup.sh

--- server-commons/php7.setup.sh
+++ server-commons/php7.setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+cd ~/vps_manager
+
 echo "Install of  PHP7"
 sudo apt-get install php7.0 php-pear libapache2-mod-php7.0 php7.0-mysql
 sudo apt-get install php7.0-curl php7.0-json php7.0-cgi
 cp /etc/php/7.0/apache2/php.ini /etc/php/7.0/apache2/php.bak.ini
 # see assets/php.conf.md
-#cp /etc/php/7.0/apache2/php.ini /vps-manager/assets/conf/php/php.ini.base
-cp /vps-manager/assets/conf/php/php.ini /etc/php/7.0/apache2/php.ini
+#cp /etc/php/7.0/apache2/php.ini assets/conf/php/php.ini.base
+cp assets/conf/php/php.ini /etc/php/7.0/apache2/php.ini
 vim /etc/php/7.0/apache2/php.ini
 sudo mkdir /var/log/php
 sudo chown www-data /var/log/php
@@ -13,3 +15,5 @@ echo "Reloading apache2..."
 sudo systemctl restart apache2
 echo "Apache2 reloaded!"
 echo "Done with php7 install."
+
+cd ~
