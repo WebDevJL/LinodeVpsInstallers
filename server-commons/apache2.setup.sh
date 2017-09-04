8470cb7af9ce31b733709b8670e100126dba01b5
Jérémie Litzler <contact@puzzlout.com>
2017-09-04 15:19:57 +0200

    Update apache2.setup.sh

--- server-commons/apache2.setup.sh
+++ server-commons/apache2.setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ~/vps-manager
 
 echo "Install of  Apache 2"
 sudo apt-get update && sudo apt-get upgrade
@@ -8,12 +9,12 @@ sudo cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bak
 # sudo vim /etc/apache2/apache2.conf
 # Set the following :
 #KeepAlive Off
-#sudo cp /etc/apache2/apache2.conf /vps-manager/assets/conf/apache2/apache2.conf
-sudo cp /vps-manager/assets/conf/apache2/apache2.conf /etc/apache2/apache2.conf
+#sudo cp /etc/apache2/apache2.conf ~/vps-manager/assets/conf/apache2/apache2.conf
+sudo cp ~/vps-manager/assets/conf/apache2/apache2.conf /etc/apache2/apache2.conf
 sudo vim /etc/apache2/apache2.conf
 
 sudo cp /etc/apache2/mods-available/mpm_prefork.conf  /etc/apache2/mods-available/mpm_prefork.back.conf
-sudo cp /vps-manager/assets/mpm_prefork.base.conf /etc/apache2/mods-available/mpm_prefork.conf
+sudo cp ~/vps-manager/assets/mpm_prefork.base.conf /etc/apache2/mods-available/mpm_prefork.conf
 #See assets/mpm_prefork.base.conf
 
 sudo a2dismod mpm_event
@@ -25,3 +26,5 @@ echo "Reloading apache2..."
 sudo systemctl reload apache2
 echo "Apache2 reloaded!"
 echo "Done with apache2 install."
+
+cd ~
