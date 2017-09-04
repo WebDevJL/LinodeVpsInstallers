ba3195149867a3643bad89b111a90e53faf9ec87
Jérémie Litzler <contact@puzzlout.com>
2017-09-04 15:18:39 +0200

    Update apache2.cache.sh

--- server-commons/apache2.cache.sh
+++ server-commons/apache2.cache.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd ~/vps-manager
+
 echo "Configuring Apache 2 caching..."
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install apache2-utils
@@ -11,3 +13,5 @@ sudo systemctl restart apache2
 # for each domain, add the configuration found in assets/sites-available/common/content-caching.conf
 echo "Now, let's test the cache..."
 ls -la /var/cache/apache2/mod_cache_disk/
+
+cd ~
