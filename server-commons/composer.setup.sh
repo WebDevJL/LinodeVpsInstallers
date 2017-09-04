a280da7208e3c50bc4963bfae5c1bb34d11fba58
Jérémie Litzler <contact@puzzlout.com>
2017-09-04 15:20:16 +0200

    Update composer.setup.sh

--- server-commons/composer.setup.sh
+++ server-commons/composer.setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+cd ~/vps-manager
+
 echo "Install of Composer"
 cd ~
 sudo apt-get install zip unzip php7.0-zip
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 composer
+
+cd ~
