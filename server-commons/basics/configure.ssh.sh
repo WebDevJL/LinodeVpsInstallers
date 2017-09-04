6951ca9725cfd3c60233fee86b8936da4affa726
Jérémie Litzler <contact@jeremielitzler.net>
2017-09-04 18:56:15 +0200

    bug fix ssh config file

--- server-commons/basics/configure.ssh.sh
+++ server-commons/basics/configure.ssh.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+cd ~/vps-manager
 
 mkdir -p ~/.ssh && sudo chmod -R 700 ~/.ssh/
-cp /vps-manager/assets/conf/ssh/sshd_config /etc/ssh/sshd_config
+cp assets/conf/ssh/sshd_config /etc/ssh/sshd_config
 vim /etc/ssh/sshd_config
 #cp /etc/ssh/sshd_config /vps-manager/assets/conf/ssh/sshd_config
 echo "Set PermitRootLogin to no"
@@ -9,3 +10,5 @@ echo "Do not set PasswordAuthentication yet"
 read -n1 -rsp $'If the sshd_config file was correct pPress any key to continue or Ctrl+C to exit...\n'
 echo 'AddressFamily inet' | sudo tee -a /etc/ssh/sshd_config
 sudo systemctl restart sshd
+
+cd ~
