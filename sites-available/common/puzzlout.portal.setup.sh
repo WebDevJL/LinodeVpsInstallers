7518eefd944fcdad3332d102b9bc035932ed638e
Jérémie Litzler <contact@puzzlout.com>
2017-09-05 15:05:39 +0200

    Update puzzlout.portal.setup.sh

--- sites-available/common/puzzlout.portal.setup.sh
+++ sites-available/common/puzzlout.portal.setup.sh
@@ -2,18 +2,21 @@
 #$1 => the domain to install
 #$2 => the UNIX user
 
+chmod -R 777 /home/$2/www
+rm -R /home/$2/www
+
 echo "Start install of Portal on $1"
-echo "Create the directories: \n /home/$2/www/public_html and /home/$2/www/logs..."
-mkdir -p /home/$2/www/{public_html,logs}
+echo "Create the directories: \n /home/$2/www/$1/public_html and /home/$2/www/$1/logs..."
+mkdir -p /home/$2/www/$1/{public_html,logs}
 ls -l /home/$2
 ls -l /home/$2/www
 read -n1 -rsp $'If all the directories created are displayed, press any key to continue otherwise Ctrl+C to exit...\n'
-echo "Browser to /home/$2/www/public_html..."
+echo "Browser to /home/$2/www/$1/public_html..."
 cd /home/$2/www/public_html
 echo "Clone the portal repo..."
 git clone https://github.com/puzzlout/portal
-echo "Copy the base index.php file in /home/$2/www/public_html/..."
-cp /home/$2/vps-manager/assets/html/index.php /home/$2/www/public_html/
+echo "Copy the base index.php file in /home/$2/www/$1/public_html/..."
+cp /home/$2/vps-manager/assets/html/index.php /home/$2/www/$1/public_html/
 echo "Set the apache conf for the portal..."
 sudo cp /home/$2/vps-manager/assets/sites-available/$1.conf /etc/apache2/sites-available/$1.conf
 echo "... and verify the copy worked..."
