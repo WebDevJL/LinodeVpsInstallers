426e73796b77a043328ec0bae5e97ca659452a4f
Jérémie Litzler <contact@puzzlout.com>
2017-09-05 11:20:17 +0200

    Update puzzlout.portal.setup.sh

--- sites-available/common/puzzlout.portal.setup.sh
+++ sites-available/common/puzzlout.portal.setup.sh
@@ -1,39 +1,54 @@
 #!/bin/bash
 #$1 => the domain to install
-echo "Install of Portal on $1"
-mkdir -p /var/www/$1/{public_html,logs}
-cd /var/www/$1/public_html
+#$2 => the UNIX user
+
+echo "Browse to home of $2 user..."
+cd /home/$2
+echo "Clone the VPS Installer repo..."
+git clone https://github.com/WebDevJL/vps-manager #clone the installer
+
+echo "Start install of Portal on $1"
+echo "Create the directories..."
+mkdir -p /home/$2/www/{public_html,logs}
+echo "Browser to /home/$2/www/public_html..."
+cd /home/$2/www/public_html
+echo "Clone the portal repo..."
 git clone https://github.com/puzzlout/portal
-cp /vps-manager/assets/html/index.php /var/www/$1/public_html/
-sudo cp /vps-manager/assets/sites-available/$1.conf /etc/apache2/sites-available/$1.conf
+echo "Copy the base index.php file in /home/$2/www/public_html/..."
+cp /home/$2/vps-manager/assets/html/index.php /home/$2/www/public_html/
+echo "Set the apache conf for the portal..."
+sudo cp /home/$2/vps-manager/assets/sites-available/$1.conf /etc/apache2/sites-available/$1.conf
+echo "... and verify the copy worked..."
 sudo vim /etc/apache2/sites-available/$1.conf
-#Generate the certs Let's Encrypt for puzzlout.ovh`
-echo "Generate the certs Let's Encrypt for $1"
-cd /opt/letsencrypt
-sudo -H ./letsencrypt-auto --apache -d $1 -d www.$1
-sudo ls /etc/letsencrypt/live #To list the certs of Let's encrypt
-sudo -H ./letsencrypt-auto --apache --renew-by-default -d $1 -d www.$1
-#echo "@monthly root /opt/letsencrypt/letsencrypt-auto --quiet --apache --renew-by-default -d $1 -d www.$1 >> /var/log/letsencrypt/letsencrypt-auto-update.log" | sudo tee --append /etc/crontab
+read -n1 -rsp $'If the conf file of $1 was not empty, press any key to continue otherwise Ctrl+C to exit...\n'
+
+sudo bash /home/$2/vps-manager/sites-available/common/website-ssl-setup.sh $1
+sudo bash /home/$2/vps-manager/sites-available/common/website-ssl-setup.sh www.$1
+
+echo "Enable $1 website..."
 sudo a2ensite $1.conf
+echo "... and finally reload Apache!"
 sudo systemctl reload apache2
 cd ~
-cd ..
 
 #Install of  phpMyAdmin for puzzlout.ovh
 echo "Install of  phpMyAdmin for $1"
-mkdir -p /var/www/sql.$1/{public_html,logs}
-cd /var/www/sql.$1/public_html
-cp /vps-manager/assets/conf/phpmyadmin/.htaccess /var/www/sql.$1/public_html/phpmyadmin/.htaccess
-cd /var/www/sql.$1/public_html
+mkdir -p /home/$2/www/sql.$1/{public_html,logs}
+cd /home/$2/www/sql.$1/public_html
+cp /home/$2/vps-manager/assets/conf/phpmyadmin/.htaccess /home/$2/www/sql.$1/public_html/phpmyadmin/.htaccess
+cd /home/$2/www/sql.$1/public_html
 sudo ln -s /usr/share/phpmyadmin
 cd ~
-cd ..
-#Install of Portal on sql.puzzlout.ovh"
-echo "Install of Portal on sql.$1"
-sudo cp /vps-manager/assets/sites-available/sql.$1.conf /etc/apache2/sites-available/sql.$1.conf
+
+sudo cp /home/$2/vps-manager/assets/sites-available/sql.$1.conf /etc/apache2/sites-available/sql.$1.conf
 sudo vim /etc/apache2/sites-available/sql.$1.conf
-sudo bash /vps-manager/sites-available/common/website-ssl-setup.sh $1
+read -n1 -rsp $'If the conf file of $1 was not empty, press any key to continue otherwise Ctrl+C to exit...\n'
+
+sudo bash /home/$2/vps-manager/sites-available/common/website-ssl-setup.sh $1
+
+echo "Enable sql.$1 website..."
 sudo a2ensite sql.$1.conf
+echo "... and finally reload Apache!"
 sudo systemctl reload apache2
 cd ~
-cd ..
+
