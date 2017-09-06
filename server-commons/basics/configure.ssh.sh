198a291080d527610c2bd7a18b964013d67999f7
Jérémie Litzler <contact@puzzlout.com>
2017-09-06 08:51:45 +0200

    Update configure.ssh.sh

--- server-commons/basics/configure.ssh.sh
+++ server-commons/basics/configure.ssh.sh
@@ -3,11 +3,11 @@ cd ~/vps-manager
 
 mkdir -p ~/.ssh && sudo chmod -R 700 ~/.ssh/
 cp assets/conf/ssh/sshd_config /etc/ssh/sshd_config
-vim /etc/ssh/sshd_config
+#vim /etc/ssh/sshd_config
 #cp /etc/ssh/sshd_config /vps-manager/assets/conf/ssh/sshd_config
 echo "Set PermitRootLogin to no"
 echo "Do not set PasswordAuthentication yet"
-read -n1 -rsp $'If the sshd_config file was correct pPress any key to continue or Ctrl+C to exit...\n'
+#read -n1 -rsp $'If the sshd_config file was correct pPress any key to continue or Ctrl+C to exit...\n'
 echo 'AddressFamily inet' | sudo tee -a /etc/ssh/sshd_config
 sudo systemctl restart sshd
 
