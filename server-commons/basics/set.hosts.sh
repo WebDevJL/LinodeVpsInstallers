71b9da71f995036c45e3b953352b40dcefcf466c
Jérémie Litzler <contact@puzzlout.com>
2017-09-06 08:58:50 +0200

    Update set.hosts.sh

--- server-commons/basics/set.hosts.sh
+++ server-commons/basics/set.hosts.sh
@@ -6,5 +6,5 @@ echo "Setting up the basics... Set hosts..."
 cp /etc/hosts /etc/hosts.bak
 echo "$1 $3" >> /etc/hosts
 echo "$2 $3" >> /etc/hosts
-vim /etc/hosts
-read -n1 -rsp $'Setting up the basics... hosts set! pPress any key to continue or Ctrl+C to exit...\n'
+#vim /etc/hosts
+#read -n1 -rsp $'Setting up the basics... hosts set! pPress any key to continue or Ctrl+C to exit...\n'
