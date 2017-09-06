05dc54acf59af989419d92420a8ed6c1adb58035
Jérémie Litzler <contact@puzzlout.com>
2017-09-06 08:59:19 +0200

    Update set.timezone.sh

--- server-commons/basics/set.timezone.sh
+++ server-commons/basics/set.timezone.sh
@@ -3,4 +3,4 @@ echo "Setting up the basics... Set hostname..."
 dpkg-reconfigure tzdata
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 date
-read -n1 -rsp $'Setting up the basics... timezone set! pPress any key to continue or Ctrl+C to exit...\n'
+#read -n1 -rsp $'Setting up the basics... timezone set! pPress any key to continue or Ctrl+C to exit...\n'
