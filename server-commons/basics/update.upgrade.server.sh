3ad6b4ea5ae375b838eed1b93ba6196e20292c2c
Jérémie Litzler <contact@puzzlout.com>
2017-09-06 09:00:11 +0200

    Update update.upgrade.server.sh

--- server-commons/basics/update.upgrade.server.sh
+++ server-commons/basics/update.upgrade.server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Setting up the basics... starting with update and upgrade of the server..."
 apt-get update && apt-get upgrade
-read -n1 -rsp $'Setting up the basics... done with update and upgrade of the server! pPress any key to continue or Ctrl+C to exit...\n'
+echo "Server up-to-date!
+#read -n1 -rsp $'Setting up the basics... done with update and upgrade of the server! pPress any key to continue or Ctrl+C to exit...\n'
