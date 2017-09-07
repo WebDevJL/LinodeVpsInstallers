262d84afd7c0bcb47ac756b60a1bd69d4d7742b4
Jérémie Litzler <contact@puzzlout.com>
2017-09-07 13:03:56 +0200

    Update puzzlout.com.setup.sh

--- sites-available/portal/puzzlout.com.setup.sh
+++ sites-available/portal/puzzlout.com.setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 vDomain="puzzlout.com"
+vUser="puzzlout"
 
 #Install of Portal on puzzlout.com"
 echo "Install of Portal on $vDomain"
-bash vps-manager/sites-available/common/puzzlout.portal.setup.sh $vDomain
+bash vps-manager/sites-available/common/puzzlout.portal.setup.sh $vDomain $vUser
