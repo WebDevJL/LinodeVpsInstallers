e9a3efacb9d4d41d0060f6d1940440f9de16689b
Jérémie Litzler <contact@jeremielitzler.net>
2017-09-07 06:56:03 +0200

    mysql setup -> removed vim

--- server-commons/mysql.setup.sh
+++ server-commons/mysql.setup.sh
@@ -7,7 +7,7 @@ sudo mysql_secure_installation
 #sudo cp /etc/mysql/mysql.conf.d/mysqld.conf /etc/mysql/mysql.conf.d/mysqld.conf.bak
 #sudo cp /etc/mysql/mysql.conf.d/mysqld.conf assets/conf/mysql/mysqld.conf
 sudo cp assets/conf/mysql/mysqld.conf /etc/mysql/mysql.conf.d/mysqld.conf
-vim /etc/mysql/mysql.conf.d/mysqld.conf
+#vim /etc/mysql/mysql.conf.d/mysqld.conf
 sudo service mysql restart
 echo "Logging in mysql-server"
 echo "(Type exit when you are done)"
